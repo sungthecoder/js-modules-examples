5b2bafe1326a4105fba2e08946d0c1455d5ed11b
Sung Choi <sungyeol.choi@gmail.com>
2016-04-05 11:25:42 -0700

    Add Script Tags

---
+++ reduce.js
@@ -0,0 +1,11 @@
+function reduce(arr, iteratee) {
+  var index = 0,
+    length = arr.length,
+    memo = arr[index];
+
+  index += 1;
+  for(; index < length; index += 1){
+    memo = iteratee(memo, arr[index])
+  }
+  return memo;
+}
